6cd84bd1536cd266652864df6dcd3aacd6955978
Priidik <priidik.vastrik@gmail.com>
2021-11-08 19:25:45 +0200

    Esimesed 12 kaarti, nende liikumine

--- proov.py
+++ proov.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -12,6 +13,7 @@ pygame.init()
 # advanced tapmine: vajutad kaardile, ilmuvad võimalused, kuhu seda kaarti saab käia, tuleb uuesti vajutada tapetava kaardi peale
 # nupp: VÕTAN, kui ei saa midagi tappa
 # käigu lõpus läheb kord järgmisele
+
 class Kaart:
     def __init__(self, mast, väärtus):
         self.mast = mast
@@ -25,7 +27,7 @@ pakk = []
 for mast in mastid:
     for kaart1 in kaardid:
         pakk.append(Kaart(mast, kaart1))
-print(pakk)
+
     
 win = pygame.display.set_mode((1200, 900))
 
@@ -41,16 +43,42 @@ bg = pygame.image.load("bg.jpg")
 card = pygame.image.load("cards/KÄrtu.png")
 scale = 4
 card = pygame.transform.scale(card, (card.get_width()/scale, card.get_height()/scale))
+laius = card.get_width()
+kõrgus = card.get_height()
 pos1x = 650
 pos1y = 400
 kaart = pygame.Rect((x, y), (card.get_width(), card.get_height()))
 liigub = False
 liikumine = 0
+kliki_counter = 0
+valitud = []
+valitudpildid = []
+valitudrect = []
+for i in range(6):
+    esimene_kaart = random.choice(pakk)
+    valitud.append(esimene_kaart)
+    pakk.remove(esimene_kaart)
+    esi_kaart = pygame.image.load(f"cards/{esimene_kaart.väärtus}{esimene_kaart.mast}.png")
+    esi_kaart = pygame.transform.scale(esi_kaart, (esi_kaart.get_width()/scale, esi_kaart.get_height()/scale))
+    kaart = pygame.Rect((200 + i*(esi_kaart.get_width()/4), 50), (esi_kaart.get_width(), esi_kaart.get_height()))
+    valitudpildid.append(esi_kaart)
+    valitudrect.append(kaart)
+
+for i in range(6):
+    esimene_kaart = random.choice(pakk)
+    valitud.append(esimene_kaart)
+    pakk.remove(esimene_kaart)
+    esi_kaart = pygame.image.load(f"cards/{esimene_kaart.väärtus}{esimene_kaart.mast}.png")
+    esi_kaart = pygame.transform.scale(esi_kaart, (esi_kaart.get_width()/scale, esi_kaart.get_height()/scale))
+    kaart = pygame.Rect((200 + i*(esi_kaart.get_width()/4), 600), (esi_kaart.get_width(), esi_kaart.get_height()))
+    valitudpildid.append(esi_kaart)
+    valitudrect.append(kaart)
 def draw_window():
     for i in range(3):
         for j in range(2):
             win.blit(bg, (i*500,j*500))
-    win.blit(card, kaart)
+    for i in range(12):
+        win.blit(valitudpildid[i], valitudrect[i])
     pygame.display.update()
     
 while True:
@@ -62,17 +90,22 @@ while True:
             exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
-            if kaart.collidepoint(pos):
-                sammx = (kaart.x-pos1x)//30
-                sammy = (kaart.y-pos1y)//30
-                liigub = True
+            for i in range(12):
+                
+                if valitudrect[i].collidepoint(pos):
+#                     sammx = (valitudrect[i].x-(pos1x-kliki_counter*(10)))/30
+                    sammx = (valitudrect[i].x-pos1x)/30
+                    sammy = (valitudrect[i].y-pos1y)//30
+                    liigub = True
+                    liikuv = valitudrect[i]
+                    kliki_counter += 1
 
                  
     keys = pygame.key.get_pressed()
 
     if liigub:
         if liikumine < 30:
-            kaart.move_ip(-sammx, -sammy)
+            liikuv.move_ip(-sammx, -sammy)
             liikumine += 1
         else:
             liigub = False
