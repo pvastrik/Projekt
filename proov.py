bb46f6183a80fbf91af9696e7b1eebf65cf0171a
Priidik <priidik.vastrik@gmail.com>
2021-11-08 18:11:06 +0200

    kaardipakk ja kommentaarid

--- proov.py
+++ proov.py
@@ -2,6 +2,31 @@ import pygame
 
 pygame.init()
 
+# kaardipakk 52 v 36 kaarti, kaardifailid listi?
+# mõlemale 6 kaarti suvaliselt (random.choice listist?) (hiljem vb kuidagi lehvikuna?) (hiljem kaardi liikumise animatsioonid?)
+# pakist järgmine kaart trump 
+# alustaja valiminine? kummal väiksem trump
+# vajutad kaardile ja see liigub väljakule alguses ainult üks, (hiljem iga kaart oma kohale)
+# algne tapmine: vajutad kaardile: kui see sobib selle kaardi peale, siis see liigub sinna
+# kui ei saa tappa, siis vajutad väljakul olevale kaardile ja see tuleb sulle kätte
+# advanced tapmine: vajutad kaardile, ilmuvad võimalused, kuhu seda kaarti saab käia, tuleb uuesti vajutada tapetava kaardi peale
+# nupp: VÕTAN, kui ei saa midagi tappa
+# käigu lõpus läheb kord järgmisele
+class Kaart:
+    def __init__(self, mast, väärtus):
+        self.mast = mast
+        self.väärtus = väärtus
+
+mastid = ["Poti", "Ärtu", "Risti", "Ruutu"]
+kaardid = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+väärtused = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 11, "Q": 12, "K": 13, "A": 14}
+
+pakk = []
+for mast in mastid:
+    for kaart1 in kaardid:
+        pakk.append(Kaart(mast, kaart1))
+print(pakk)
+    
 win = pygame.display.set_mode((1200, 900))
 
 pygame.display.set_caption("Turakas")
